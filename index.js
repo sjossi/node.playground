93c6d5af3db30462f7e870b215fc851c6ae7a5f8
Savino Jossi <jossi@pressanykey.ch>
2013-02-27 00:25:08 +0100

    added stuff here and there

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-var http = require("http");
+// var http = require("http");
 
-function onRequest(request, response) {
-    console.log("Request received");
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World!");
-    response.end();
+// function onRequest(request, response) {
+//     console.log("Request received");
+//     response.writeHead(200, {"Content-Type": "text/plain"});
+//     response.write("Hello World!");
+//     response.end();
 
-}
+// }
 
-http.createServer(onRequest).listen(8888);
+// http.createServer(onRequest).listen(8888);
 
-console.log("Server is running at http://localhost:8888");
+// console.log("Server is running at http://localhost:8888");
+
+var server = require("./server.js")
+
+server.start();
